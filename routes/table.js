e41074639f9b91e2fa01fc1ec30041f28685e0e9
Raunak Mandal <iamriju2000@gmail.com>
2022-01-23 19:20:52 +0530

    Concurrently added

--- routes/table.js
+++ routes/table.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { addTable, tableByuser } = require("../controllers/table");
 const router = express.Router();
-const Table = require("../models/table");
 router.post("/addTable", addTable);
-
 router.get("/tables/:user", tableByuser);
 module.exports = router;
