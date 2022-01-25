a88612d34293fde90a61bdf439faa368ffcf74f3
Raunak Mandal <iamriju2000@gmail.com>
2022-01-25 08:36:48 +0530

    ok

---
+++ client/build/precache-manifest.361f3f4a39f052f3c39a4072b3bc46f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e1449b7032aece55b6bd081bf4c274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3bde3cb24689153227f",
+    "url": "/static/css/main.a3ef7ed3.chunk.css"
+  },
+  {
+    "revision": "42279b0b139a09d3b9f5",
+    "url": "/static/js/2.7ba41dec.chunk.js"
+  },
+  {
+    "revision": "8e804db7f366fe46ed3501cfcc3667fa",
+    "url": "/static/js/2.7ba41dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bde3cb24689153227f",
+    "url": "/static/js/main.e75a2c42.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
