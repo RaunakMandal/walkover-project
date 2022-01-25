9d81ab30140c3d5762e28acf3cf0045f039d4c87
Raunak Mandal <iamriju2000@gmail.com>
2022-01-25 09:08:13 +0530

    not wrk 2

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", route);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build")); // serve the static react app
+  app.get(/^\/(?!api).*/, (req, res) => {
+    // don't serve api routes to react app
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  console.log("Serving React App...");
 }
 
 mongoose
